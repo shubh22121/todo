bc1932b654622c70b1dbea61234f120ed76d8c07
shubham <shubham22121@gmail.com>
2022-01-09 18:06:32 +0530

    todo app 📝

---
+++ todo/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+
+from todo.models import Todo
+
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+
+    if request.method == 'POST':
+        new_todo = Todo(title=request.POST['title'])
+        new_todo.save()
+        return redirect('/')
+
+    return render(request, 'index.html', {'todos': todos})
+
+
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
